e5f4581e55846da6b706fa0fc045d39d49c81e5c
Shane Connon <shane.connon@gmail.com>
2018-09-22 19:25:34 +0100

    Added new constructor to ObjectPropertyGraph

--- SqlDsl.UnitTests/DataParser/RootObjectPropertyGraphTests.cs
+++ SqlDsl.UnitTests/DataParser/RootObjectPropertyGraphTests.cs
@@ -15,6 +15,7 @@ using SqlDsl.UnitTests.FullPathTests.Environment;
 using SqlDsl.Sqlite;
 using NUnit.Framework.Interfaces;
 using SqlDsl.DataParser;
+using SqlDsl.Query;
 
 namespace SqlDsl.UnitTests.DataParser
 {
@@ -30,7 +31,7 @@ namespace SqlDsl.UnitTests.DataParser
             public List<Tag> Tags { get; set; }
         }
 
-        static Dsl.IQuery<JoinedQueryClass> FullyJoinedQuery()
+        static QueryBuilder<Sqlite.SqliteBuilder, JoinedQueryClass> FullyJoinedQuery()
         {
             return Sql.Query.Sqlite<JoinedQueryClass>()
                 .From<Person>(x => x.ThePerson)
@@ -41,13 +42,15 @@ namespace SqlDsl.UnitTests.DataParser
                 .InnerJoin<ClassTag>(q => q.ClassTags)
                     .On((q, ct) => q.Classes.One().Id == ct.ClassId)
                 .InnerJoin<Tag>(q => q.Tags)
-                    .On((q, t) => q.ClassTags.One().TagId == t.Id);
+                    .On((q, t) => q.ClassTags.One().TagId == t.Id) as QueryBuilder<Sqlite.SqliteBuilder, JoinedQueryClass>;
         }
 
         void Compare(ObjectPropertyGraph x, ObjectPropertyGraph y)
         {
             if (x == null && y == null) return;
             if (x == null || y == null) Fail();
+
+            CompareRowIdColumnNumbers(x, y);
 
             if (x.SimpleProps.Count() != y.SimpleProps.Count()) Fail("Simple props count");
             for (var i = 0; i < x.SimpleProps.Count(); i++)
@@ -78,6 +81,53 @@ namespace SqlDsl.UnitTests.DataParser
                 message = message == null ? "" : (" " + message);
                  Assert.Fail($"Objects are not equal:{message}\n{x}\n{y}");
             }
+        }
+
+        void CompareRowIdColumnNumbers(ObjectPropertyGraph x, ObjectPropertyGraph y)
+        {
+            if (x == null && y == null) return;
+            if (x == null || y == null) Fail();
+
+            if (x.RowIdColumnNumbers.Count() != y.RowIdColumnNumbers.Count()) Fail("RowIdColumnNumbers count");
+            for (var i = 0; i < x.RowIdColumnNumbers.Count(); i++)
+            {
+                if (x.RowIdColumnNumbers.ElementAt(i) !=
+                    y.RowIdColumnNumbers.ElementAt(i))
+                    Fail("RowIdColumnNumber " + i);
+            }
+
+            if (x.ComplexProps.Count() != y.ComplexProps.Count()) Fail("Complex props count");
+            for (var i = 0; i < x.ComplexProps.Count(); i++)
+            {
+                var x_ = x.ComplexProps.ElementAt(i);
+                var y_ = y.ComplexProps.ElementAt(i);
+
+                if (x_.name != y_.name)
+                    Fail("Complex prop " + i);
+
+                CompareRowIdColumnNumbers(x_.value, y_.value);
+            }
+
+            void Fail(string message = null) 
+            {
+                message = message == null ? "" : (" " + message);
+                 Assert.Fail($"Objects are not equal:{message}\n{x}\n{y}");
+            }
+        }
+
+        [Test]
+        [Ignore("TODO")]
+        public void PropertyGraph_WithRootAndJoins_CreatesCorrectRowIdColumnNumbers()
+        {
+            // arrange
+            // act
+            var result = FullyJoinedQuery()
+                .ToSqlBuilder(null)
+                .builder
+                .BuildObjetPropertyGraph(typeof(JoinedQueryClass));
+
+            // assert
+            CompareRowIdColumnNumbers(new ObjectPropertyGraph(null, null, null), result.graph);
         }
     }
 }
