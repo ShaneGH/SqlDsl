28cf271da4b0e0616c9c05505f61154c32096849
Shane Connon <shane.connon@gmail.com>
2018-09-30 10:07:15 +0100

    extra tests

---
+++ SqlDsl.UnitTests/FullPathTests/MappingTestsWithFunLogic.cs
@@ -0,0 +1,112 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using SqlDsl.Utils;
+using SqlDsl.UnitTests.FullPathTests.Environment;
+using SqlDsl.Sqlite;
+using NUnit.Framework.Interfaces;
+
+namespace SqlDsl.UnitTests.FullPathTests
+{
+    [TestFixture]
+    public class MappingTestsWithFunLogic : FullPathTestBase
+    {
+        class JoinedQueryClass
+        {
+            public Person ThePerson { get; set; }
+            public List<PersonClass> PersonClasses { get; set; }
+            public List<Class> Classes { get; set; }
+            public List<ClassTag> ClassTags { get; set; }
+            public List<Tag> Tags { get; set; }
+        }
+
+        static Dsl.IQuery<JoinedQueryClass> FullyJoinedQuery()
+        {
+            return Sql.Query.Sqlite<JoinedQueryClass>()
+                .From<Person>(x => x.ThePerson)
+                .InnerJoin<PersonClass>(q => q.PersonClasses)
+                    .On((q, pc) => q.ThePerson.Id == pc.PersonId)
+                .InnerJoin<Class>(q => q.Classes)
+                    .On((q, c) => q.PersonClasses.One().ClassId == c.Id)
+                .InnerJoin<ClassTag>(q => q.ClassTags)
+                    .On((q, ct) => q.Classes.One().Id == ct.ClassId)
+                .InnerJoin<Tag>(q => q.Tags)
+                    .On((q, t) => q.ClassTags.One().TagId == t.Id);
+        }
+
+        [Test]
+        [Ignore("TODO")]
+        public async Task SimpleMapOn1FullTable()
+        {
+            // arrange
+            // act
+            var data = await FullyJoinedQuery()
+                .Map(p => p.ThePerson)
+                .ExecuteAsync(Executor);
+
+            // assert
+            Assert.AreEqual(2, data.Count());
+            Assert.AreEqual(Data.People.John, data.First());
+            Assert.AreEqual(Data.People.Mary, data.ElementAt(1));
+        }
+
+        [Test]
+        [Ignore("TODO")]
+        public async Task SimpleMapOn1Table()
+        {
+            // arrange
+            // act
+            var data = await FullyJoinedQuery()
+                .Map(p => p.ThePerson.Name)
+                .ExecuteAsync(Executor);
+
+            // assert
+            Assert.AreEqual(2, data.Count());
+            Assert.AreEqual(Data.People.John.Name, data.First());
+            Assert.AreEqual(Data.People.Mary.Name, data.ElementAt(1));
+        }
+
+        [Test]
+        [Ignore("TODO")]
+        public async Task MapWithAddition()
+        {
+            // arrange
+            // act
+            var data = await FullyJoinedQuery()
+                .Map(p => p.ThePerson.Id + 1)
+                .ExecuteAsync(Executor);
+
+            // assert
+            Assert.AreEqual(2, data.Count());
+            Assert.AreEqual(Data.People.John.Id + 1, data.First());
+            Assert.AreEqual(Data.People.Mary.Id + 1, data.ElementAt(1));
+        }
+
+        [Test]
+        [Ignore("TODO")]
+        public async Task MapWithAddition2()
+        {
+            // arrange
+            var one = 1;
+
+            // act
+            var data = await FullyJoinedQuery()
+                .Map(p => p.ThePerson.Id + one)
+                .ExecuteAsync(Executor);
+
+            // assert
+            Assert.AreEqual(2, data.Count());
+            Assert.AreEqual(Data.People.John.Id + 1, data.First());
+            Assert.AreEqual(Data.People.Mary.Id + 1, data.ElementAt(1));
+        }
+    }
+}
--- SqlDsl.UnitTests/FullPathTests/MultiJoinedMappingTests.cs
+++ SqlDsl.UnitTests/FullPathTests/MultiJoinedMappingTests.cs
@@ -27,32 +27,51 @@ namespace SqlDsl.UnitTests.FullPathTests
             public List<Class> Classes { get; set; }
             public List<ClassTag> ClassTags { get; set; }
             public List<Tag> Tags { get; set; }
-            public List<Purchase> Purchases { get; set; }
+            public List<Purchase> PurchasesByMe { get; set; }
+            public List<Purchase> PurchasesByMeForMyClasses { get; set; }
         }
 
-        // [Test]
-        // public async Task SimpleMapOn1Table()
-        // {
-        //     // arrange
-        //     // act
-        //     var data = await Sql.Query.Sqlite<Person>()
-        //         .From()
-        //         .Map(p => new SimpleMapClass
-        //         { 
-        //             TheName = p.Name,
-        //             Inner = new SimpleMapClass
-        //             {
-        //                 TheName = p.Name
-        //             }
-        //         })
-        //         .ExecuteAsync(Executor);
+        static Dsl.IQuery<JoinedQueryClass> FullyJoinedQuery()
+        {
+            return Sql.Query.Sqlite<JoinedQueryClass>()
+                .From<Person>(x => x.ThePerson)
+                .LeftJoin<PersonClass>(q => q.PersonClasses)
+                    .On((q, pc) => q.ThePerson.Id == pc.PersonId)
+                .LeftJoin<Class>(q => q.Classes)
+                    .On((q, c) => q.PersonClasses.One().ClassId == c.Id)
+                .LeftJoin<ClassTag>(q => q.ClassTags)
+                    .On((q, ct) => q.Classes.One().Id == ct.ClassId)
+                .LeftJoin<Tag>(q => q.Tags)
+                    .On((q, t) => q.ClassTags.One().TagId == t.Id)
+                .LeftJoin<Purchase>(q => q.PurchasesByMe)
+                    .On((q, t) => q.ThePerson.Id == t.PersonId)
+                .LeftJoin<Purchase>(q => q.PurchasesByMeForMyClasses)
+                    .On((q, t) => q.ThePerson.Id == t.PersonId && q.Classes.One().Id == t.ClassId);
+        }
 
-        //     // assert
-        //     Assert.AreEqual(2, data.Count());
-        //     Assert.AreEqual(Data.People.John.Name, data.First().TheName);
-        //     Assert.AreEqual(Data.People.John.Name, data.First().Inner.TheName);
-        //     Assert.AreEqual(Data.People.Mary.Name, data.ElementAt(1).TheName);
-        //     Assert.AreEqual(Data.People.Mary.Name, data.ElementAt(1).Inner.TheName);
-        // }
+        [Test]
+        [Ignore("TODO")]
+        public async Task SimpleMapOn1Table()
+        {
+            // arrange
+            // act
+            var data = await FullyJoinedQuery()
+                // .Map(p => new SimpleMapClass
+                // { 
+                //     TheName = p.Name,
+                //     Inner = new SimpleMapClass
+                //     {
+                //         TheName = p.Name
+                //     }
+                // })
+                .ExecuteAsync(Executor);
+
+            // assert
+            // Assert.AreEqual(2, data.Count());
+            // Assert.AreEqual(Data.People.John.Name, data.First().TheName);
+            // Assert.AreEqual(Data.People.John.Name, data.First().Inner.TheName);
+            // Assert.AreEqual(Data.People.Mary.Name, data.ElementAt(1).TheName);
+            // Assert.AreEqual(Data.People.Mary.Name, data.ElementAt(1).Inner.TheName);
+        }
     }
 }
