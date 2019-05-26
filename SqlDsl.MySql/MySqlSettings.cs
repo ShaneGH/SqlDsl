8648dc878d05b1ef182d139e51c9200b89bc42d4
Shane Connon <shane.connon@gmail.com>
2019-05-26 22:37:44 +0100

    added mysql row_number support

--- SqlDsl.MySql/MySqlSettings.cs
+++ SqlDsl.MySql/MySqlSettings.cs
@@ -17,7 +17,7 @@ namespace SqlDsl
 
         public static readonly MySqlSettings Default = new MySqlSettings
         {
-            Version8OrHigher = true
+            Version8OrHigher = false
         };
     }
 }
