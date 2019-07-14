e4db7129b37d0734c98cb78b01ee5e88815eb017
Shane Connon <shane.connon@gmail.com>
2019-07-14 14:18:32 +0100

    x

---
+++ SqlDsl.TSql/TSql.cs
@@ -0,0 +1,40 @@
+using SqlDsl.Dsl;
+using SqlDsl.Query;
+using SqlDsl.TSql;
+
+namespace SqlDsl
+{
+    /// <summary>
+    /// Utils to build and execute TSql queries
+    /// </summary>
+    public static class TSqlUtils
+    {
+        static readonly TSqlSyntax TSqlSyntax = new TSqlSyntax(TSqlSettings.Default);
+
+        /// <summary>
+        /// Build a TSql query
+        /// </summary>
+        /// <param name="strictJoins">If set to true, every join added to the SqlDsl query will also be added to the Sql query.
+        /// If false, joins which are not used in a mapping, WHERE clause, ON clause etc... will be automatically removed</param>
+        /// <param name="settings">The settings for the query. If null, TSqlSettings.Default are used</param>
+        public static ISqlSelect<TResult> TSql<TResult>(this QueryBuilder builder, bool strictJoins = true, TSqlSettings settings = null)
+        {
+            return new SqlSelect<TResult>(
+                settings == null ? TSqlSyntax : new TSqlSyntax(settings), 
+                strictJoins);
+        }
+
+        /// <summary>
+        /// Build a TSql query with args
+        /// </summary>
+        /// <param name="strictJoins">If set to true, every join added to the SqlDsl query will also be added to the Sql query.
+        /// If false, joins which are not used in a mapping, WHERE clause, ON clause etc... will be automatically removed</param>
+        /// <param name="settings">The settings for the query. If null, TSqlSettings.Default are used</param>
+        public static ISqlSelect<TArgs, TResult> TSql<TArgs, TResult>(this QueryBuilder builder, bool strictJoins = true, TSqlSettings settings = null)
+        {
+            return new SqlSelect<TArgs, TResult>(
+                settings == null ? TSqlSyntax : new TSqlSyntax(settings), 
+                strictJoins);
+        }
+    }
+}
---
+++ SqlDsl.TSql/TSqlExecutor.cs
@@ -0,0 +1,63 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using TSql.Data.TSqlClient;
+
+namespace SqlDsl.TSql
+{
+    /// <summary>
+    /// An object which can execute a TSql query
+    /// </summary>
+    public class TSqlExecutor : IExecutor
+    {
+        /// <summary>
+        /// The connection
+        /// </summary>
+        public TSqlConnection Connection;
+
+        private static readonly Regex AllowUserVariables1 = new Regex(@"AllowUserVariables\s*=\s*True", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+        private static readonly Regex AllowUserVariables2 = new Regex(@"Allow\s+User\s+Variables\s*=\s*True", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+        public TSqlExecutor(TSqlConnection connection)
+        {
+            Connection = connection ?? throw new ArgumentNullException(nameof(connection));
+
+            if (!AllowUserVariables1.IsMatch(Connection.ConnectionString) && !AllowUserVariables2.IsMatch(Connection.ConnectionString))
+                throw new InvalidOperationException("Your connection string must include \"Allow User Variables=True\".");
+        }
+
+        /// <inheritdoc />
+        public async Task<IReader> ExecuteAsync(string sql, IEnumerable<(string name, object value)> paramaters) =>
+            new TSqlReader(await CreateCommand(sql, paramaters).ExecuteReaderAsync().ConfigureAwait(false));
+
+        /// <inheritdoc />
+        public IReader Execute(string sql, IEnumerable<(string name, object value)> paramaters) =>
+            new TSqlReader(CreateCommand(sql, paramaters).ExecuteReader());
+
+        /// <inheritdoc />
+        public Task ExecuteCommandAsync(string sql, IEnumerable<(string name, object value)> paramaters) =>
+            CreateCommand(sql, paramaters).ExecuteNonQueryAsync();
+
+        /// <inheritdoc />
+        public void ExecuteCommand(string sql, IEnumerable<(string name, object value)> paramaters) =>
+            CreateCommand(sql, paramaters).ExecuteNonQuery();
+
+        /// <summary>
+        /// Create a sql command with the given sql and parameters
+        /// </summary>
+        TSqlCommand CreateCommand(string sql, IEnumerable<(string name, object value)> paramaters)
+        {
+            var command = Connection.CreateCommand();
+            command.CommandText = sql;
+
+            foreach (var p in paramaters)
+            {
+                command.Parameters.Add(new TSqlParameter(p.name, p.value ?? DBNull.Value));
+            }
+
+            return command;
+        }
+    }
+}
